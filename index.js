b9572e01be2f9e89c242d2bfc230fc633f847aa2
yomi odusi <yomiodusi98@gmail.com>
2022-06-15 01:56:55 -0400

    completed add dept and add role functionality

--- index.js
+++ index.js
@@ -27,10 +27,12 @@ function init() {
             name: "options",
             type: "list",
             message: "What would you like to do?",
+            pageSize: 10,
             choices: [
                 "View All Departments",
                 "View All Roles", 
                 "View All Employees", 
+                "Add a dept", 
                 "Add a role", 
                 "Add an Employee", 
                 "Update an Employee Role",
@@ -50,6 +52,10 @@ function init() {
                 case "View All Employees":
                     viewEmp();
                     break;
+
+                case "Add a dept":
+                    addDept();
+                    break;    
 
                 case "Add a role":
                     addRole();
@@ -75,7 +81,7 @@ init();
 //Viewing All Departments
 function viewDep() {
     connection.query(` SELECT * FROM emp_directory.department`, (err, res)=>{
-        return console.table(res)
+         return console.table(res)
     })
     init();
 }
@@ -91,9 +97,82 @@ function viewRole() {
 //Viewing All Employees
 function viewEmp() {
     connection.query(` SELECT * FROM emp_directory.employees`, (err, res)=>{
-        return console.table(res)
+       return  console.table(res)
     })
     init();
 }
 
+//Add a Department
+function addDept() {
+    inquirer
+        .prompt({
+            name: "dept_name",
+            type: "input",
+            message: "What is the new department called?"
+        }, )
+        .then(function(response) {
+            connection.query(`INSERT INTO department (dept_name) VALUES (?)`, [response.dept_name], (err, res)=>{
+                return console.table(res)
+            })
+            init();
+        })
+}
 
+
+//Add a Role
+function addRole() {
+    inquirer
+        .prompt([{
+            name: "title",
+            type: "input",
+            message: "What is this new title called?"
+        }, {
+            name: "salary",
+            type: "input",
+            message: "What is the salary for this new title?"
+        }, {
+            name: "dept_id",
+            type: "input",
+            message: "What is the dept id for this new title?"
+        }, ])
+        .then(function(response) {
+            connection.query(`INSERT INTO emp_role (title,salary,dept_id) VALUES (?,?,?)`, [response.title, response.salary, response.dept_id], (err, res)=>{
+                return console.table(res)
+            })
+            init();
+        })
+}
+
+
+//Add an Employee
+function addEmp() {
+    inquirer
+        .prompt([{
+            name: "firstName",
+            type: "input",
+            message: "What is the employee's first name?"
+        }, {
+            name: "lastName",
+            type: "input",
+            message: "What is the employee's last name?"
+        }, {
+            name: "role_id",
+            type: "input",
+            message: "What is the employee's role id?"
+        }, {
+            name: "manager_id",
+            type: "input",
+            message: "What is the employees manager's id?",
+
+        }, ])
+        .then(function(answer) {
+            let query = "INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES (?,?,?,?)"
+            connection.query(query, [answer.firstName, answer.lastName, parseInt(answer.role_id), parseInt(answer.manager_id)], function(err, res) {
+                if (err) throw err;
+                console.log(res);
+            })
+            init();
+        })
+}
+
+//Update an Employee Role
